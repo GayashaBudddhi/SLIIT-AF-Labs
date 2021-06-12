806c161fcd6d5f589634dd838078d48e1bb4763e
GayashaBudddhi <it19061580@my.sliit.lk>
2021-06-12 13:13:03 +0530

    Lab 7 added

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Posts from "./components/Posts";
+
+const posts = [
+    {
+        id: 1,
+        name: 'React',
+        description: 'Best UI library'
+    }, {
+        id: 2,
+        name: 'Node',
+        description: 'Server side JS'
+    }
+];
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return <div>
+            <Posts posts={posts}/>
+        </div>;
+    }
+};
+
+
